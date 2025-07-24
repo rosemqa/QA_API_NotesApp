0ed96ee97df89066e5b9cd1103e0dd37838e630e
rosemqa <ers6wobmo09m@mail.ru>
2025-07-24 21:32:30 +0300

    minor fixes

--- config/base_test.py
+++ config/base_test.py
@@ -3,9 +3,6 @@ from services.users.users_api import UserAPI
 
 
 class BaseTest:
-    # def __init__(self):
-    #     self.api_user = None
-
     def setup_method(self):
         self.api_user = UserAPI()
         self.api_notes = NotesAPI()
